c36a47c8d81f9b4bbe7ec50ecf360e99c7455e35
Abbos-coder <khamidullaev1998@gmail.com>
2022-05-15 09:47:15 +0500

    routes fix and add user id on models

--- models/product.js
+++ models/product.js
@@ -1,48 +1,55 @@
 const mongoose = require("mongoose");
 const Joi = require("joi");
+// const { string } = require("joi");
 
 const productSchema = new mongoose.Schema({
-   category: {
-      type: String,
-      required: true,
-   },
-   image: {
-      type: Object,
-      default: "",
-      required: true,
-   },
-   title: {
-      type: String,
-      required: true,
-      trim: true,
-   },
-   price: {
-      type: Number,
-      required: true,
-      trim: true,
-   },
-   rating: {
-      type: Number,
-      default: 4.5,
-      trim: true,
-      required: false,
-   },
-   status: {
-      type: Boolean,
-      required: true,
-      default: false,
-   },
+  userId: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: Object,
+    default: "",
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+    trim: true,
+    required: false,
+  },
+  status: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
 });
 
 function validateProduct(product) {
-   const schema = Joi.object({
-      category: Joi.string().required(),
-      title: Joi.string().required(),
-      price: Joi.number().required(),
-      status: Joi.boolean().required(),
-      rating: Joi.number(),
-   });
-   return schema.validate(product);
+  const schema = Joi.object({
+    userId: Joi.string().required(),
+    category: Joi.string().required(),
+    title: Joi.string().required(),
+    price: Joi.number().required(),
+    status: Joi.boolean().required(),
+    rating: Joi.number(),
+  });
+  return schema.validate(product);
 }
 
 const Product = mongoose.model("Product", productSchema);
